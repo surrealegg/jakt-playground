74cbd9b1f90aabe8e0e3568cc56bf77b775b15ef
surrealegg <surrealegg@pm.me>
2022-05-22 18:57:36 +0400

    Add timeout for most commands
    This will prevent from container hanging
    Also add prune command for docker I might remove this later

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
-
-run() {
-    $@
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
+#!/bin/bash
 
 CODEGEN=${CODEGEN:-0};
+
+run() {
+    timeout 5 $@
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        if [ $exit_code -eq 124 ]; then
+            >&2 echo "Timeout while running '$@'"
+        fi
+        exit $exit_code
+    fi
+}
 
 run jakt input.jakt
 
@@ -16,6 +20,6 @@ if [ $CODEGEN -eq 1 ]; then
     exit 0
 fi
 
-run clang++ -std=c++20 -I/usr/local/include/runtime -Wno-user-defined-literals /playground/output.cpp
-/playground/a.out
+run clang++ -std=c++20 -I/usr/local/include/runtime -Wno-user-defined-literals -fcolor-diagnostics /playground/output.cpp
+run /playground/a.out
 rm -f /playground/a.out /playground/output.cpp
--- sandbox/setup.sh
+++ sandbox/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+docker image prune
 docker build -t 'jakt_sandbox' ./sandbox
 docker images
